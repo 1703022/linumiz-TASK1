93db60fdb017eb2105f8e2239f694e9102383a65
karthik p s <86886498+1703022@users.noreply.github.com>
2021-07-29 08:16:29 +0000

    Create arth.c

---
+++ arth.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "arth.h"
+int main()
+{
+long long int a,b;
+printf("enter two numbers for addtion:\n");
+scanf("%lld\n%lld",&a,&b);
+add(a,b);
+sub(a,b);
+mul(a,b);
+return 0;
+}
+# linumiz-TASK1
